3fb3ba29d3884e9d17aae2d415f9ad9449dd1794
JWJ <1393020517@qq.com>
2019-04-10 20:50:25 +0800

    整合页面1

--- 后台管理系统/js/main.js
+++ 后台管理系统/js/main.js
@@ -3,31 +3,6 @@
 /*登录弹窗*/
 
 
-function start() {
-    document.getElementById('signform').style.display=""
-}
-
-function signclose() {
-    document.getElementById('signform').style.display="none";
-    document.getElementById('registerform').style.display="none"
-}
-/********************************/
-/*跳转页面*/
-var user_list = document.getElementById('user_list');
-user_list.onclick=function () {
-    location="./user.html"
-}
-
-var exam = document.getElementById('exam');
-exam.onclick=function () {
-    location="./examine.html"
-}
-
-var pic = document.getElementById('pic');
-pic.onclick=function () {
-    location="./picture.html"
-}
-
 
 
 
@@ -62,23 +37,18 @@ turn.onclick=function() {
 
 
                 if (data.status) {
-                    document.getElementById('load').style.display="none";
-                    document.getElementById('pic').style.display="";
-                    document.getElementById('exam').style.display="";
-                    document.getElementById('user_list').style.display="";
 
-                    location = "./main.html?id="+username/*待修改*/
+
+                    location = "./examine.html"/*待修改*/
                 }
                 else {
-                    document.getElementById('signform').style.display="none";
-
                     alert('用户名或密码错误')
                 }
 
 
             },
             error:function(){
-                document.getElementById('signform').style.display="none";
+
                 alert('无法连接服务器')
             }
         })
